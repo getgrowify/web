ad30c19b9383c12d164f724fc4427783c1b5384f
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-05-23 11:29:34 -0400

    Update cards.jsx

--- src/views/pages/cards.jsx
+++ src/views/pages/cards.jsx
@@ -38,9 +38,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1416879595882-3373a0480b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -49,9 +49,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1495107334309-fcf20504a5ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -60,9 +60,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1593179835737-9f82a5f30c68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -71,9 +71,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1585499193151-0f50d54c4e1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3152&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -84,9 +84,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1536657464919-892534f60d6e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3153&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -95,9 +95,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1486328228599-85db4443971f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -106,9 +106,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
@@ -117,9 +117,9 @@ const Cards = () => {
                     <Card>
                         <CardImg top width="100%" src={"https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80"}/>
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
-                            <CardSubtitle>Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                            <CardTitle>Post Title Test!</CardTitle>
+                            <CardSubtitle>Post Description</CardSubtitle>
+                            <CardText>Post Content</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
